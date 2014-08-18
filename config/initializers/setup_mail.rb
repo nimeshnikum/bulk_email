45b7ccf2127c9c5a9c5bd499b2d679bbcaf9feba
nimeshnikum <nimesh.nikum@gmail.com>
2014-08-19 00:08:37 +0530

    Configured hostgator SMTP

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,15 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-#  :domain               => "trulynx.com",
-  :user_name            => "trulynxmarketing",
-  :password             => "m@rketing",
+  :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
+  :address              => "mail.trulynx.net",
+  :domain               => "trulynx.net",
+  :user_name            => "marketing@trulynx.net",
+  :password             => "%M30Z}a2C_?R",
   :authentication       => "plain",
   :enable_starttls_auto => true
+#  :address              => "smtp.gmail.com",
+#  :port                 => 587,
+#  :user_name            => "trulynxmarketing",
+#  :password             => "m@rketing",
 }
 
 #Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
